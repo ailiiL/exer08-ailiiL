37c8ca0ee5326a3f57b7fa37c05ddbe887fafa06
ailiiL <tplegayo@up.edu.ph>
2026-04-18 15:50:16 +0800

    chore: add expense_provider

--- my_app/lib/providers/expense_provider.dart
+++ my_app/lib/providers/expense_provider.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/api/firebase_expenses_api.dart';
-import '../../../../models/todo_model.dart';
+import '../models/expense_model.dart';
 
 class ExpensesListProvider with ChangeNotifier {
   late Stream<QuerySnapshot> _expensesStream;
@@ -13,7 +13,7 @@ class ExpensesListProvider with ChangeNotifier {
   }
 
   // getter
-  Stream<QuerySnapshot> get todo => _expensesStream;
+  Stream<QuerySnapshot> get expense => _expensesStream;
 
   // TODO: get all todo items from Firestore
   void fetchExpenses() {
@@ -22,22 +22,22 @@ class ExpensesListProvider with ChangeNotifier {
   }
 
   // TODO: add todo item and store it in Firestore
-  Future<void> addTodo(Todo item) async {
-    String message = await firebaseService.addTodo(item.toJson());
+  Future<void> addExpense(Expense item) async {
+    String message = await firebaseService.addExpense(item.toJson());
     print(message);
     notifyListeners();
   }
 
   // TODO: edit a todo item and update it in Firestore
-  Future<void> editTodo(String id, String newTitle) async {
-    String message = await firebaseService.editTodo(id,newTitle);
+  Future<void> editExpense(String id, String newName, String newDesc, String newCategory, int newAmount) async {
+    String message = await firebaseService.editExpense(id, newName, newDesc, newCategory, newAmount);
     print(message);
     notifyListeners();
   }
 
   // TODO: delete a todo item and update it in Firestore
-  Future<void> deleteTodo(String id) async {
-    String message = await firebaseService.deleteTodo(id);
+  Future<void> deleteExpense(String id) async {
+    String message = await firebaseService.deleteExpense(id);
     print(message);
     notifyListeners();
   }
